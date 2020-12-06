8f368a2576df8086ef0777572e20239b9beaaaeb
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-12-06 12:08:37 +0200

    Добавляет стили для формы register

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-import loginMarkup from '../templates/login.hbs'
+import loginMarkup from '../templates/login.hbs';
+import registerMarkup from '../templates/register.hbs';
 
 const mainPage = document.querySelector('body');
 
-mainPage.insertAdjacentHTML('afterbegin', loginMarkup());
+// mainPage.insertAdjacentHTML('afterbegin', loginMarkup());
+mainPage.insertAdjacentHTML('afterbegin', registerMarkup());
 
