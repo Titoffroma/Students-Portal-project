9fe0f4ad671b6ca157114d188f2aaa85d41535a1
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2020-12-06 16:41:13 +0200

    init branch

---
+++ src/js/getRefs.js
@@ -0,0 +1,15 @@
+  
+const refs = {
+  formEditProfile: document.querySelector('.form-edit-profile'),
+  btnEditProfile: document.querySelector('.btn-edit-profile'),
+  avatarImgInput: document.querySelector('#avatar-img-add'),
+  btnLoadImg: document.querySelector('.btn-load-img'),
+  btnDeleteImg: document.querySelector('.btn-delete-img'),
+  btnSkillDelete: document.querySelector('.btn-skill-delete'),
+  formSkillsAdd: document.querySelector('.form-skills-add'),
+  btnSkillAdd: document.querySelector('.btn-skill-add'),
+  formAboutMe: document.querySelector('.form-about-me'),
+  btnAboutMeAdd: document.querySelector('.btn-add-about-me')
+};
+
+export default refs; 
---
+++ src/js/my-profile.js
@@ -0,0 +1,13 @@
+import './sass/main.scss';
+import 'material-design-icons/iconfont/material-icons.css';
+import { load, save, remove  } from './storage';
+import { notificationCenter } from './notification-center/index';
+import { pushError, removeError } from './pnotify';
+import refs from './getRefs';
+import myProfileTpl from '../templates/my-profile.hbs';
+
+
+
+
+
+
